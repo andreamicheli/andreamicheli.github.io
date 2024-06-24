800ccc3f35098d5968c26a5411eccd225cb3f03b
Andrea Micheli <amicheli@kth.se>
2024-06-24 16:41:34 +0200

    deployment fix?

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
 
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+  // webpack5: true,
+  // webpack: (config) => {
+  //   config.resolve.fallback = { fs: false };
 
-    return config;
-  },
+  //   return config;
+  // },
 };
